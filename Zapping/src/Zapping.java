c5a696763eb3f109dbf391539da91f24704e262a
Daniel Pantoja <daanielpantoja@gmail.com>
2023-05-12 17:55:07 +0200

    segunda refactorización

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,12 +1,6 @@
 public class Zapping {
-    public static String zap(int canalA, int canalB){
+    public static String zap(int canalA, int canalB) {
         int zaps;
-
-
-        if (canalA == canalB){
-            zaps = 0;
-        }
-
 
         if (canalA < 50 && canalB > 50) {
             zaps = (canalA + 99) - canalB;
