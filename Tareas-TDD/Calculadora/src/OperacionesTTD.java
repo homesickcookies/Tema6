7561f57341efa2b76575fd6c02e5cb2ea5a2f43a
Daniel Pantoja <daanielpantoja@gmail.com>
2023-05-12 16:20:28 +0200

    test (null), (1), (1,1) OK

--- Tareas-TDD/Calculadora/src/OperacionesTTD.java
+++ Tareas-TDD/Calculadora/src/OperacionesTTD.java
@@ -1,2 +1,14 @@
 public class OperacionesTTD {
+    public static int suma(String numbers){
+        int result = 0;
+        if (!numbers.equals("")) {
+            String[] inputs = numbers.split(",");
+
+            for (String number : inputs) {
+                result += Integer.parseInt(number);
+            }
+        }
+
+        return result;
+    }
 }
--- Tareas-TDD/Calculadora/src/OperacionesTTDTest.java
+++ Tareas-TDD/Calculadora/src/OperacionesTTDTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperacionesTTDTest {
-
+    @ParameterizedTest
+    @CsvSource({
+            "'', 0",
+            "'1', 1",
+            "'1,2', 3",
+    })
+    void testCalculadora(String input, int resultadoEsperado){
+        int resultadoReal = OperacionesTTD.suma(input);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
 }
