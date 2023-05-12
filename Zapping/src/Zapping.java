a7d3e9198ed7a2615e0ba8f51293967917bd3a00
Daniel Pantoja <daanielpantoja@gmail.com>
2023-05-12 17:25:21 +0200

    Test 1 OK

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,2 +1,11 @@
 public class Zapping {
+    public static String zap(int canalA, int canalB){
+        if (canalA == canalB){
+            return "0 clics";
+        }
+
+        String result = "";
+
+        return result;
+    }
 }
--- Zapping/src/ZappingTest.java
+++ Zapping/src/ZappingTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class ZappingTest {
-
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, '0 clics'",
+//            "1, 2, '1 clics'",
+//            "10, 30, '20 clics'",
+//            "2, 99, '2 clics'",
+//            "5, 63, '41 clics'",
+//            "63, 5, '41 clics'"
+    })
+    void testCalculadora(int input0, int input1, String resultadoEsperado){
+        String resultadoReal = Zapping.zap(input0, input1);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
 }
