8b47222e259e4cffca79e7ba85c15e6ce0a0e934
Daniel Pantoja <daanielpantoja@gmail.com>
2023-05-22 22:55:30 +0200

    no funciona el CSV y estoy testeando cosas

--- SongTTD/src/TTDSongs.java
+++ SongTTD/src/TTDSongs.java
@@ -1,14 +1,20 @@
-import java.util.ArrayList;
-
 public class TTDSongs {
-    public static String ttdSongs(String animal) {
-        String song = "TEST";
+    public static String ttdSongs(String animals) {
+        String song = "";
+        if (animals == null) {
+            return song;
+        }
+        String[] yard = animals.split(",");
 
-        if (animal == null) {
-            return "";
+        if (yard.length == 1) {
+            song += "There was an old lady who swallowed a " + yard[0] + ".\n" +
+                    "I don't know why she swallowed a " + yard[0] + " - perhaps she'll die!" +
+                    "\n";
         }
 
-
         return song;
+
     }
+
+
 }
--- SongTTD/src/TTDSongsTest.java
+++ SongTTD/src/TTDSongsTest.java
@@ -1,16 +1,35 @@
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TTDSongsTest {
     @ParameterizedTest
-    @CsvSource({
-            ",''",
-    })
-
+//    @CsvFileSource(delimiter = '|', resources = "/testData/tasca7.csv")
+    @CsvFileSource(delimiter = '|', Te)
     void testTTDSongs(String input, String resultadoEsperado) {
         String resultadoReal = TTDSongs.ttdSongs(input);
         assertEquals(resultadoEsperado, resultadoReal);
     }
 }
+
+
+
+/*
+"There was an old lady who swallowed a bird.\n" +
+"I don't know why she swallowed a bird - perhaps she'll die!\n" +
+"\n" +
+"There was an old lady who swallowed a fly;\n" +
+"That wriggled and wiggled and tickled inside her.\n" +
+"She swallowed the fly to catch the bird;\n" +
+"I don't know why she swallowed a bird - perhaps she'll die!\n" +
+"\n" +
+"There was an old lady who swallowed a spider;\n" +
+"How absurd to swallow a spider.\n" +
+"She swallowed the spider to catch the fly,\n" +
+"She swallowed the fly to catch the bird;\n" +
+"I don't know why she swallowed a bird - perhaps she'll die!\n" +
+"\n"
+
+ */
